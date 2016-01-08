fdd35f1a2c0b121498d7b9746b240f40de7cf118
ttissot <thierry.tissot@exacompta.com>
2016-01-08 18:17:05 +0100

    Create connect.php

---
+++ config/connect.php
@@ -0,0 +1,5 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+$GLOBALS['spip_connect_version'] = 0.4;
+spip_connect_db('bhojqtgja.mysql.clvrcld.net','3306','uaczu12jwyxouxm0','7954JpvJMOnX5P2K26f','bhojqtgja');
+?>
